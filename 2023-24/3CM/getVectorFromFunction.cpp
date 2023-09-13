60f1997776b67dba0aa105c2395616f030e56c82
robwarsz <robertwarszawski@gmail.com>
2023-09-13 16:11:56 +0200

    Add files via upload

---
+++ 2023-24/3CM/getVectorFromFunction.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> getDivisors(int number) {	
+   	vector<int> V;
+	for (int i=1 ; i*i<=number ; i++)
+		if (number%i==0) {
+			V.push_back(i);
+			if (number/i!=i) 
+			V.push_back(number/i);
+		}	
+	sort(V.begin(),V.end());			
+	return V;
+}
+
+int main()
+{
+	int number;
+	cout<<"Podaj liczbe: ";
+	cin>>number;
+	cout<<"\nDzielniki liczby " << number << ": ";
+    vector<int> Divisors = getDivisors(number);
+    for(auto el : Divisors) 
+        cout<<el << ", ";     
+    return 0;
+}
