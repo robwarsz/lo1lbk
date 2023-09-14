75d19350224ec892755163bd54814e843d373dbc
robwarsz <robertwarszawski@gmail.com>
2023-09-14 08:42:41 +0200

    Add files via upload

---
+++ 2023-24/3CM/getPrimeNumbers.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> getPrimeFactors(int number) {
+	vector<int> primeFactors;
+	for (int i=2 ; i*i<=number ; i++ )
+		while (number%i==0) {
+			primeFactors.push_back(i);
+			number/=i;
+		}
+	if (number!=1)
+		primeFactors.push_back(number);
+	return primeFactors;	
+}
+
+int main() {
+	int number;
+	cin>>number;
+	vector<int> primeFactors = getPrimeFactors(number);
+	for (auto el:primeFactors)
+		cout << el << ",";
+}
