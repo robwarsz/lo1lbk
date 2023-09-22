47a1b88aef4e2368b0ed7f9773143d2ec054be1c
robwarsz <robertwarszawski@gmail.com>
2023-09-22 14:07:55 +0200

    Add files via upload

--- 2023-24/3CM/zadCKE/zad58/rozw.cpp
+++ 2023-24/3CM/zadCKE/zad58/rozw.cpp
@@ -8,11 +8,35 @@ vector<pair<int,int>> V2;
 vector<pair<int,int>> V3;
 
 int AnyToDec(string,int);
+void readData() {
+	ifstream in1 ("dane_systemy1.txt");	
+	for (int i=0 ; i<1095 ; i++) {
+		string a,b;
+		in1 >> a >> b;
+		V1.push_back(make_pair(AnyToDec(a,2),AnyToDec(b,2)));
+	}		
+	in1.close();
+	ifstream in2 ("dane_systemy2.txt");	
+	for (int i=0 ; i<1095 ; i++) {
+		string a,b;
+		in2 >> a >> b;
+		V2.push_back(make_pair(AnyToDec(a,4),AnyToDec(b,4)));
+	}		
+	in2.close();
+	ifstream in3 ("dane_systemy3.txt");	
+	for (int i=0 ; i<1095 ; i++) {
+		string a,b;
+		in3 >> a >> b;
+		V3.push_back(make_pair(AnyToDec(a,8),AnyToDec(b,8)));
+	}		
+	in3.close();
+}
 
 int main() {
-	cout << AnyToDec("-1111",2)<<endl;
-	cout << AnyToDec("-123123",4)<<endl;
-	cout << AnyToDec("123123",4)<<endl;
+	readData();
+	cout << V1[21].first << " " << V1[21].second<<endl;
+	cout << V2[21].first << " " << V2[21].second<<endl;
+	cout << V3[21].first << " " << V3[21].second;
 }
 
 int AnyToDec(string S, int sys) {
